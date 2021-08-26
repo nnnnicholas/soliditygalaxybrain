8e745df1027260fb3272c5da8ef94aa69e6d8715
Nicholas <nicholas@nftstory.life>
2021-08-26 01:11:56 -0400

    opengraph

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,6 @@ import RSS from '../public/rss.png';
 
 
 function App() {
-  // Create the count state.
-  const [count, setCount] = useState(0);
-  // Create the counter (+1 every second).
-  useEffect(() => {
-    const timer = setTimeout(() => setCount(count + 1), 1000);
-    return () => clearTimeout(timer);
-  }, [count, setCount]);
-  // Return the App component.
   return (
     <div className={style.App}>
       <div id={style.imageContainer}>
